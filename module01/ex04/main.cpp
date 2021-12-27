986bc57c0e850d30ff24fbc791beb65ec6662b5f
Yassin Ait kaddour <yait-kad@e2r2p2.1337.ma>
2021-12-27 16:20:55 +0100

    ex04

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -10,4 +10,43 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <fstream>
 
+std::string uppercase(std::string str)
+{
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        str[i] = toupper(str[i]);
+    }
+    return (str);
+    
+}
+
+int main(int ac, char **args)
+{
+    if (ac != 4)
+    {
+        std::cout<<"error: number of arguments!!"<<std::endl;
+        return (0);
+    }
+    std::string file;
+    std::string s1;
+    std::string s2;
+    
+    file = args[1];
+    s1 = args[2];
+    s2 = args[3];
+
+    std::ifstream infile(file);
+    if (!infile.is_open())
+    {
+        std::cout<<"error: file not exist!!"<<std::endl;
+        return (0);
+    }
+    std::ofstream outfile;
+    outfile.open(uppercase(file) + ".replace");
+    
+    
+    return (0);
+}
