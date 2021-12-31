f58f249bdf6f47f599cd85ecc7c47000b894aac8
Yassin Ait kaddour <yait-kad@e2r2p2.1337.ma>
2021-12-31 15:29:38 +0100

    ex05

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -23,6 +23,14 @@ std::string uppercase(std::string str)
     
 }
 
+void replacing(std::string s1, std::string s2, int found, std::string &line)
+{
+    size_t len = s1.length();
+
+    line.erase(found, len);
+    line.insert(found, s2);
+}
+
 int main(int ac, char **args)
 {
     if (ac != 4)
@@ -30,9 +38,12 @@ int main(int ac, char **args)
         std::cout<<"error: number of arguments!!"<<std::endl;
         return (0);
     }
+    std::string line;
     std::string file;
     std::string s1;
     std::string s2;
+    int i;
+    size_t found;
     
     file = args[1];
     s1 = args[2];
@@ -46,7 +57,27 @@ int main(int ac, char **args)
     }
     std::ofstream outfile;
     outfile.open(uppercase(file) + ".replace");
-    
+
+    while (getline(infile, line))
+    {
+        i = 0;
+        while (line[i])
+        {
+            found = line.find(s1,i);
+            if (found != std::string::npos)
+            {
+                replacing(s1, s2, found, line);
+                i = found + s2.length();
+            }
+            else
+                i++;
+        }
+        outfile << line;
+		if (!infile.eof())
+			outfile << std::endl;
+    }
+    infile.close();
+    outfile.close();
     
     return (0);
 }
